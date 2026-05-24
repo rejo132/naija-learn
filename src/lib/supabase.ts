c4cb30c75721803657c19bf4197c5d4e4af64cef
Enoch <enochrejesho254@gmail.com>
2026-05-24 16:25:32 +0300

    fixed supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -28,6 +28,6 @@ export const supabase = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_ANON_KEY, {
     storage: getStorage(),
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: true,
   },
 });
